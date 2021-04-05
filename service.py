4d3e37e49a0b17ac5e6503dd5ff6a94be2838873
CHEMLI Mohamed Ali (INTERN) <MCI22@dsone.3ds.com>
2021-04-05 11:39:10 +0200

    update

--- service.py
+++ service.py
@@ -49,5 +49,17 @@ def get_specefic_statistics():
     extractedData = jsonFileFromdB[key_]
     return extractedData
 
+@app.route('/user_init', methods=["POST"])
+def get_specefic_statistics():
+    user = "userID"
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/quiz_list", [])
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/avg_score" , 0)
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/number_quiz_done" , 0)
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/number_correct_qcm", 0)
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/number_qcm_done", 0)
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/number_qcm_done_faculte", 0)
+    firebase.post('ionicapp-2bb5c-default-rtdb/'+ user + "/quiz/number_qcm_done_year", 0)
+    return "OK - initialized"
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
